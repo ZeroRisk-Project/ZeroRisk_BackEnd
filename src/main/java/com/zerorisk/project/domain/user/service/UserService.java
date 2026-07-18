4fcc6d2770f28c566b123b2ab10de7e5bd7bb6ff
jongkwon0918 <whdrnjs0918@naver.com>
2026-07-19 02:19:01 +0900

    feat: 회원가입 서비스 로직 구현

---
+++ src/main/java/com/zerorisk/project/domain/user/service/UserService.java
@@ -0,0 +1,42 @@
+package com.zerorisk.project.domain.user.service;
+
+import com.zerorisk.project.domain.user.dto.SignupRequest;
+import com.zerorisk.project.domain.user.dto.SignupResponse;
+import com.zerorisk.project.domain.user.entity.User;
+import com.zerorisk.project.domain.user.repository.UserRepository;
+import com.zerorisk.project.global.exception.DuplicateEmailException;
+import com.zerorisk.project.global.exception.DuplicateNicknameException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignupResponse signup(SignupRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new DuplicateEmailException();
+        }
+        if (userRepository.existsByNickname(request.nickname())) {
+            throw new DuplicateNicknameException();
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        User user = User.builder()
+                .email(request.email())
+                .nickname(request.nickname())
+                .password(encodedPassword)
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        return new SignupResponse(savedUser.getId(), savedUser.getEmail(), savedUser.getNickname());
+    }
+}
