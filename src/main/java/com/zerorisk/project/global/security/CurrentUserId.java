c3f19fadd488f17c5ad075c12b3fb797d28bb177
jongkwon0918 <whdrnjs0918@naver.com>
2026-07-19 20:09:24 +0900

    feat: CurrentUserId 커스텀 어노테이션 추가

---
+++ src/main/java/com/zerorisk/project/global/security/CurrentUserId.java
@@ -0,0 +1,13 @@
+package com.zerorisk.project.global.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this")
+public @interface CurrentUserId {
+}
