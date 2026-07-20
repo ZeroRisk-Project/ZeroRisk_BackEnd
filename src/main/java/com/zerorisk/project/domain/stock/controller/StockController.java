88d082219ee363cc74cca552ed145704a26cd26c
5s-mini <dnclsehd123@naver.com>
2026-07-21 00:36:58 +0900

    feat: StockController에 키워드 검색 엔드포인트 추가

--- src/main/java/com/zerorisk/project/domain/stock/controller/StockController.java
+++ src/main/java/com/zerorisk/project/domain/stock/controller/StockController.java
@@ -1,12 +1,17 @@
 package com.zerorisk.project.domain.stock.controller;
 
 import com.zerorisk.project.domain.stock.dto.StockDetailResponse;
+import com.zerorisk.project.domain.stock.dto.StockSummaryResponse;
 import com.zerorisk.project.domain.stock.service.StockQueryService;
+import com.zerorisk.project.domain.stock.service.StockSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class StockController {
 
     private final StockQueryService stockQueryService;
+    private final StockSearchService stockSearchService;
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<StockSummaryResponse>> search(
+            @RequestParam String keyword,
+            Pageable pageable) {
+        return ResponseEntity.ok(stockSearchService.search(keyword, pageable));
+    }
 
     @GetMapping("/{code}")
     public ResponseEntity<StockDetailResponse> getDetail(@PathVariable String code) {
