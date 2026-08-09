1f49b3b158f3b1ed6f848afe6a290be96c6cb0a3
jongkwon0918 <whdrnjs0918@naver.com>
2026-08-09 14:14:19 +0900

    fix: 회원 관리 API에 관리자 권한 검증 누락된 보안 취약점 수정

--- src/main/java/com/zerorisk/project/domain/user/controller/AdminUserController.java
+++ src/main/java/com/zerorisk/project/domain/user/controller/AdminUserController.java
@@ -2,6 +2,7 @@ package com.zerorisk.project.domain.user.controller;
 
 import com.zerorisk.project.domain.user.dto.AdminUserResponse;
 import com.zerorisk.project.domain.user.dto.UserSuspendRequest;
+import com.zerorisk.project.domain.user.entity.UserStatus;
 import com.zerorisk.project.domain.user.service.AdminUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // TODO: SecurityConfig에서 /api/v1/admin/** 경로에 ADMIN 권한 체크 적용 필요 (박종권님 설정 확인)
@@ -23,8 +25,11 @@ public class AdminUserController {
     private final AdminUserService adminUserService;
 
     @GetMapping
-    public Page<AdminUserResponse> getUsers(Pageable pageable) {
-        return adminUserService.getUsers(pageable);
+    public Page<AdminUserResponse> getUsers(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) UserStatus status,
+            Pageable pageable) {
+        return adminUserService.getUsers(keyword, status, pageable);
     }
 
     @PatchMapping("/{userId}/suspend")
