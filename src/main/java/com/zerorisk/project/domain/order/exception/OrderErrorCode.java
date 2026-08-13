fb10b1af3606b5da7a32e12891a574150f8d3a64
5s-mini <dnclsehd123@naver.com>
2026-08-13 13:30:16 +0900

    feat: 주문 에러 코드 작성

---
+++ src/main/java/com/zerorisk/project/domain/order/exception/OrderErrorCode.java
@@ -0,0 +1,18 @@
+package com.zerorisk.project.domain.order.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum OrderErrorCode {
+
+    LIMIT_PRICE_REQUIRED("ORDER_001", "지정가 주문은 가격을 입력해야 합니다.", HttpStatus.BAD_REQUEST),
+    INSUFFICIENT_BALANCE("ORDER_002", "주문 가능 금액이 부족합니다.", HttpStatus.BAD_REQUEST),
+    INSUFFICIENT_HOLDING("ORDER_003", "보유 수량이 부족합니다.", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus status;
+}
