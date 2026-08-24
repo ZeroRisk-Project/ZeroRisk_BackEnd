3f95a0fcea704531a68e8c9a43b29c2ec15eccaa
5s-mini <dnclsehd123@naver.com>
2026-08-24 15:56:24 +0900

    feat: 기간별 조회 메서드 추가

--- src/main/java/com/zerorisk/project/domain/portfolio/repository/PortfolioSnapshotRepository.java
+++ src/main/java/com/zerorisk/project/domain/portfolio/repository/PortfolioSnapshotRepository.java
@@ -2,9 +2,13 @@ package com.zerorisk.project.domain.portfolio.repository;
 
 import com.zerorisk.project.domain.portfolio.entity.PortfolioSnapshot;
 import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PortfolioSnapshotRepository extends JpaRepository<PortfolioSnapshot, Long> {
 
     boolean existsByAccountIdAndSnapshotDate(Long accountId, LocalDate snapshotDate);
+
+    List<PortfolioSnapshot> findByAccountIdAndSnapshotDateBetweenOrderBySnapshotDateAsc(
+            Long accountId, LocalDate from, LocalDate to);
 }
