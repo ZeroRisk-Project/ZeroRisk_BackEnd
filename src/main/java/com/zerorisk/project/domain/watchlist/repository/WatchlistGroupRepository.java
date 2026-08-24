b70c22078fb3ead5f6eb0d11f4a500fb904d9380
5s-mini <dnclsehd123@naver.com>
2026-08-24 19:22:19 +0900

    feat: 사용자별 목록 조회 메서드 추가

--- src/main/java/com/zerorisk/project/domain/watchlist/repository/WatchlistGroupRepository.java
+++ src/main/java/com/zerorisk/project/domain/watchlist/repository/WatchlistGroupRepository.java
@@ -1,8 +1,9 @@
 package com.zerorisk.project.domain.watchlist.repository;
 
 import com.zerorisk.project.domain.watchlist.entity.WatchlistGroup;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WatchlistGroupRepository extends JpaRepository<WatchlistGroup, Long> {
-
+    List<WatchlistGroup> findByUserId(Long userId);
 }
