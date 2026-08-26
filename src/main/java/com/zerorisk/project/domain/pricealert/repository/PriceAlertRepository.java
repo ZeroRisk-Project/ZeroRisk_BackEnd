4c7ffaafcb1adefedb88f1a8202dbb0cf3fe3212
5s-mini <dnclsehd123@naver.com>
2026-08-26 16:31:31 +0900

    feat: 사용자별 조회 메서드 추가

--- src/main/java/com/zerorisk/project/domain/pricealert/repository/PriceAlertRepository.java
+++ src/main/java/com/zerorisk/project/domain/pricealert/repository/PriceAlertRepository.java
@@ -1,8 +1,9 @@
 package com.zerorisk.project.domain.pricealert.repository;
 
 import com.zerorisk.project.domain.pricealert.entity.PriceAlert;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PriceAlertRepository extends JpaRepository<PriceAlert, Long> {
-
+    List<PriceAlert> findByUserId(Long userId);
 }
