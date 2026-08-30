fef24c874a389e8a54b6a944ee442c3a50a800da
오상민 <dnclsehd123@naver.com>
2026-08-31 02:38:18 +0900

    Revert "[Chat] WebSocket 세션 관리 및 강제 종료 파이프라인"

--- src/main/java/com/zerorisk/project/global/config/RedisConfig.java
+++ src/main/java/com/zerorisk/project/global/config/RedisConfig.java
@@ -1,21 +1,13 @@
 package com.zerorisk.project.global.config;
 
-import com.zerorisk.project.global.websocket.RedisDisconnectSubscriber;
-import com.zerorisk.project.global.websocket.SessionDisconnectPublisher;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@RequiredArgsConstructor
 public class RedisConfig {
-
-    private final RedisDisconnectSubscriber redisDisconnectSubscriber;
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -24,14 +16,5 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
-    }
-
-    @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(
-                redisDisconnectSubscriber, new ChannelTopic(SessionDisconnectPublisher.CHANNEL));
-        return container;
     }
 }
